4bd592c0151d630629f3c77ac277100ccef08575
leonelcampos <leonelcampos@MacBook-Pro-de-Leonel.local>
2018-04-12 18:12:06 -0300

    Proyecto unity con scripts para usar el aceleromtro

---
+++ gyroscope1/Assets/AcceleratorRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcceleratorRotation : MonoBehaviour
+{
+
+	private float Speed = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(Input.acceleration.y, Input.acceleration.x, 0) * Time.deltaTime * Speed);
+	}
+}
