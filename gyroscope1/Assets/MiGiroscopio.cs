464fd15d2f4634c90a565cb7ac7a5761a2895c24
gabrielpalacios <palaciosgabriel@hotmail.com>
2018-04-18 10:24:28 -0300

    Add files via upload

---
+++ gyroscope1/Assets/MiGiroscopio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiGiroscopio : MonoBehaviour {
+
+	Gyroscope m_Gyro;
+	// Use this for initialization
+	void Start()
+	{
+		//Set up and enable the gyroscope (check your device has one)
+		m_Gyro = Input.gyro;
+		m_Gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Input.gyro.attitude;
+	}
+}
