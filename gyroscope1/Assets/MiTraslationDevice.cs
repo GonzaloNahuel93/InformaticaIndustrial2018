d3660f2007a6e1060ba8b99ee6a6d3cde0f28114
palaciosgabriel@hotmail.com <palaciosgabriel@hotmail.com>
2018-05-17 00:02:34 -0300

    lectura de posicion inicial para giroscopio

--- gyroscope1/Assets/MiTraslationDevice.cs
+++ gyroscope1/Assets/MiTraslationDevice.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class MiTraslationDevice : MonoBehaviour
 {
-	[SerializeField] private float Speed = 4f;
+	[SerializeField] private float Speed = 2f;
 
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 dir = Vector3.zero;
-		dir.x = Math.Abs(Input.acceleration.x) > 0.1 ? -Input.acceleration.y : 0;
-		dir.y = Math.Abs(Input.acceleration.y) > 0.1 ? -Input.acceleration.x : 0;
+		dir.x = Math.Abs(Input.acceleration.x) > 0.001 ? -Input.acceleration.y : 0;
+		dir.y = Math.Abs(Input.acceleration.y) > 0.001 ? -Input.acceleration.x : 0;
 		if (dir.sqrMagnitude > 1)
 		{
 			dir.Normalize();
