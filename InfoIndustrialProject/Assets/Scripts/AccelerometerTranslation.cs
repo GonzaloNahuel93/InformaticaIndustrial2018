0c155f73d4186664fb760611038380ba73b964db
leonel.campos <leonelcampos@MacBook-Pro-de-Leonel.local>
2018-05-31 14:39:06 -0300

    Se modifica todo el proyecto

---
+++ InfoIndustrialProject/Assets/Scripts/AccelerometerTranslation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AccelerometerTranslation : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+	{	
+		transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+	}
+}
---
+++ InfoIndustrialProject/Assets/Scripts/GyroscopeRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GyroscopeRotation : MonoBehaviour {
+
+	Gyroscope m_Gyro;
+	public Vector3 baseAcceleration;
+	
+	void Start()
+	{
+		m_Gyro = Input.gyro;
+		m_Gyro.enabled = true;
+	}
+	
+	void Update () {
+		Vector3 lect = Input.gyro.attitude.eulerAngles;
+		Quaternion q = Quaternion.Euler((lect.x - baseAcceleration.x),
+			-(lect.y - baseAcceleration.y), 
+			-(lect.z - baseAcceleration.z));
+		transform.rotation = q;
+	}
+}
