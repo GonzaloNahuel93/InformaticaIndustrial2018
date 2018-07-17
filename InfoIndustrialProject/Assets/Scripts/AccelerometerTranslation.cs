79100dd2a13120dae37c8d87c285c18a76700a33
leonel.campos <leonelcampos@MacBook-Pro-de-Leonel.local>
2018-07-17 01:43:54 -0300

    Se arregla código para acumular distancias

--- InfoIndustrialProject/Assets/Scripts/AccelerometerTranslation.cs
+++ InfoIndustrialProject/Assets/Scripts/AccelerometerTranslation.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class AccelerometerTranslation : MonoBehaviour {
+public class AccelerometerTranslation : MonoBehaviour
+{
+    Vector3 startPoint;
+    float ajuste = 0.4f;
+    float meters;
+    public Text _Text;
 
-	Vector3 startPoint;
-	string meterLabel;
-	float ajuste = 0.4f;
-	float meters;
+    void Start()
+    {
+        startPoint = transform.position;
+    }
 
-	void Start(){
-		startPoint = transform.position;
-	}
-
-	void OnGUI () {
-		GUIStyle style = new GUIStyle ();
-		style.richText = true;
-		GUI.Label (new Rect (80, 80, 200, 100), "<size=30>Distancia: " + meterLabel + "</size>", style);
-	}
-
-	void Update () {
-		float metersX = transform.position.x - startPoint.x;
-		float metersY = transform.position.y - startPoint.y;
-		float metersZ = transform.position.z - startPoint.z;
-		meters = Mathf.Abs (metersX) + Mathf.Abs (metersY) + Mathf.Abs (metersZ);
-		meterLabel = meters.ToString("C");
-		transform.Translate(Input.acceleration.x * ajuste, Input.acceleration.y * 0, -Input.acceleration.z * ajuste, Space.World);
-	}
+    void Update()
+    {
+        if (startPoint != transform.position)
+        {
+            float metersX = transform.position.x - startPoint.x;
+            float metersY = transform.position.y - startPoint.y;
+            float metersZ = transform.position.z - startPoint.z;
+            meters = meters + Mathf.Abs(metersX) + Mathf.Abs(metersY) + Mathf.Abs(metersZ);
+            startPoint = transform.position;
+            _Text.text = "Distancia recorrida: " + meters.ToString("C") + " Metros";
+            transform.Translate(Input.acceleration.x * ajuste, Input.acceleration.y * 0, -Input.acceleration.z * ajuste,
+                Space.World);
+            startPoint = transform.position;
+        }
+    }
 }
--- InfoIndustrialProject/Assets/Scripts/Distance.cs
+++ InfoIndustrialProject/Assets/Scripts/Distance.cs
@@ -7,15 +7,10 @@ public class Distance : MonoBehaviour {
 	public Text _Text;
 	public float _Distance;
 	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update ()
 	{
-		_Text.text = _Distance.ToString("0") + " Metros";
+		_Text.text = "Distancia a la esfera: "+ _Distance.ToString("0") + " Metros";
 		_Distance = Vector3.Distance(_ControlPoint.position, transform.position);
 	}
 }
